5a99491d50fa60b12c0a1bd835e574f86808d0a7
dong <1031342303@qq.com>
2020-03-12 15:24:55 +0800

    3-12 update

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,35 +1,90 @@
 <template>
-  <div>
-    <child-nav-bar></child-nav-bar>
-    <child-swiper :top-images="topImages"></child-swiper>
+  <div id="detail">
+    <nav-bar class="nav-bar"></nav-bar>
+    <scroll class="content" ref="scroll">
+      <swiper :top-images="topImages"></swiper>
+      <detail-baseInfo :goods="goods"></detail-baseInfo>
+      <shop-info :shop="shopInfo"></shop-info>
+      <detail-goodsInfo
+        :detailInfo="detailInfo"
+        @imageLoad="imageLoad"
+      ></detail-goodsInfo>
+    </scroll>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+
+.content {
+  position: absolute;
+  top: 44px;
+  bottom: 60px;
+}
+.nav-bar {
+  position: relative;
+  background-color: #fff;
+  z-index: 1;
+}
+</style>
 
 <script>
-import ChildNavBar from './components/ChildNavBar';
-import ChildSwiper from './components/ChildSwiper';
+import NavBar from './components/NavBar';
+import Swiper from './components/Swiper';
+import DetailBaseInfo from './components/DetailBaseInfo';
+import ShopInfo from './components/ShopInfo';
+import DetailGoodsInfo from './components/DetailGoodsInfo';
 
-import { getDetail } from 'network/detail';
+import Scroll from 'components/common/scroll/Scroll';
+
+import { getDetail, Goods, Shop } from 'network/detail';
 export default {
   name: 'Detail',
-  components: { ChildNavBar, ChildSwiper },
+  components: {
+    NavBar,
+    Swiper,
+    DetailBaseInfo,
+    ShopInfo,
+    DetailGoodsInfo,
+    Scroll,
+  },
   data() {
     return {
       id: null,
       topImages: [],
+      goods: {},
+      shopInfo: {},
+      detailInfo: {},
     };
   },
   watch: {},
   computed: {},
-  methods: {},
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh();
+    },
+  },
   created() {
     console.log('进入了Detail页面');
     this.id = this.$route.params.id;
     getDetail(this.id).then(res => {
-      // console.log(res);
-      this.topImages = res.result.itemInfo.topImages;
+      const data = res.result;
+      //获取顶部轮播数据
+      this.topImages = data.itemInfo.topImages;
+      //获取商品信息
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
+      //创建店铺信息
+      this.shopInfo = new Shop(data.shopInfo);
+      this.detailInfo = data.detailInfo;
     });
   },
 };
---
+++ src/views/detail/components/DetailBaseInfo.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="detail-Info" v-if="Object.keys(goods).length !== 0">
+    <div class="title">
+      {{ goods.title }}
+    </div>
+    <div class="detail-center">
+      <span>{{ goods.newPrice }}</span>
+      <span>{{ goods.oldPrice }}</span>
+      <span>{{ goods.discount }}</span>
+    </div>
+    <div class="detail-bottom">
+      <!-- <span class="flex-item">销量1580</span>
+      <span class="flex-item">收藏33人</span>
+      <span class="flex-item">72小时发货</span> -->
+      <span
+        class="flex-item"
+        v-for="index in goods.services.length - 1"
+        :key="index"
+      >
+        <img :src="goods.services[index].icon" />
+        <span>{{ goods.services[index - 1].name }}</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.detail-Info {
+  font-size: 12px;
+}
+.title {
+  margin: 10px 5px;
+  font-weight: 700;
+}
+.detail-center span {
+  margin: 0 5px;
+}
+.detail-center span:first-child {
+  font-size: 16px;
+  color: #ff6677;
+}
+
+.detail-center span:nth-child(2) {
+  text-decoration: line-through;
+}
+.detail-center span:last-child {
+  background-color: #ff6677;
+  color: #ffffff;
+  padding: 3px;
+  border-radius: 10px;
+}
+
+.detail-bottom {
+  margin: 30px 5px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.detail-bottom .flex-item {
+  margin: 0 2px;
+}
+.flex-item img {
+  height: 10px;
+}
+</style>
+
+<script>
+export default {
+  name: 'DetailBaseInfo',
+  props: {
+    goods: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  components: {},
+  data() {
+    return {};
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() { }
+};
+</script>
